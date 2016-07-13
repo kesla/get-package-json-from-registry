085d3aae983af14d4ea7829320b05263152c0946
David Bjorklund <david.bjorklund@gmail.com>
2016-07-14 01:04:04 +0200

    allow arg to be parsed arg

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 import test from 'tapava';
 import getPackageJsonFromRegistry from './lib';
+import npa from 'npm-package-arg';
 
-test('getPackageJsonFromRegistry(), user/repo#ref', function * (t) {
+test('getPackageJsonFromRegistry(), package@version', function * (t) {
   const packageJson = yield getPackageJsonFromRegistry('snappy@5.0.0');
   t.match(packageJson, {
     _requested: {
@@ -18,7 +19,24 @@ test('getPackageJsonFromRegistry(), user/repo#ref', function * (t) {
   });
 });
 
-test('cached, user/repo#ref', function * (t) {
+test('getPackageJsonFromRegistry(), package@version from npa object', function * (t) {
+  const packageJson = yield getPackageJsonFromRegistry(npa('snappy@5.0.0'));
+  t.match(packageJson, {
+    _requested: {
+      raw: 'snappy@5.0.0',
+      scope: null,
+      escapedName: 'snappy',
+      name: 'snappy',
+      rawSpec: '5.0.0',
+      spec: '5.0.0',
+      type: 'version'
+    },
+    name: 'snappy',
+    version: '5.0.0'
+  });
+});
+
+test('cached, package@version', function * (t) {
   const cached = getPackageJsonFromRegistry.cached();
   const packageJson = yield cached('snappy@5.0.0');
   t.match(packageJson, {
